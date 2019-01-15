84a3517816540507c2ef2c2734bbbe1c7a56e0fc
Stefan Mach <smach@iis.ee.ethz.ch>
2019-01-15 16:50:58 +0100

    :art: [pipe] Rewrite pipe to avoid Verilator bug

--- src/fpnew_pipe_out.sv
+++ src/fpnew_pipe_out.sv
@@ -12,6 +12,25 @@
 // (http://www.pulp-platform.org), under the copyright of ETH Zurich and the
 // University of Bologna.
 
+// Generate pipeline stages as given by NumPipeRegs. When NumPipeRegs is 0, no registers are
+// generated.
+//            +---------|---------|---------|----------|-------------------+
+//            |  _d[0]  |  _d[1]  |  _d[2]  |  _d[..]  |  _d[NumPipeRegs]  |
+//            |         |  _q[0]  |  _q[1]  | _q[..-1] | _q[NumPipeRegs-1] |
+// inputs_i   >=========|=========|=========|====~~====|===================> inputs_o
+// in_valid_i >---------|---------|---------|----~~----|-------------------> out_valid_o
+// in_ready_o <---------+---------+---------+----~~----+-------------------< out_ready_i
+//            |         |         |         |          |                   |
+// stage #    +----0----|----1----|----2----|----..----|----NumPipeRegs----+
+//
+// NOTE: These registers must be retimed in synthesis for sensible pipelining. Make sure to
+// optimize registers through the instantiating hierarchy.
+// The ready signal is not a direct feed-through from destination to source but takes into account
+// intermediate 'bubbles' in the pipeline. As such, downstream stalls can be hidden when the
+// pipeline is not full.
+// Enable signals on the registers will lead to clock-gated pipeline stages when this optimization
+// is enabled during synthesis. Make sure to optimize clock gates through hierarchies.
+
 // Author: Stefan Mach <smach@iis.ee.ethz.ch>
 
 `include "registers.svh"
@@ -47,78 +66,81 @@ module fpnew_pipe_out #(
   output logic               busy_o
 );
 
-  // Pipelined versions of signals for each stage (one more than number of registers)
-  logic [0:NumPipeRegs][Width-1:0]    result_q;
-  fpnew_pkg::status_t [0:NumPipeRegs] status_q;
-  logic [0:NumPipeRegs]               extension_bit_q;
-  TagType [0:NumPipeRegs]             tag_q;
-  AuxType [0:NumPipeRegs]             aux_q;
-  logic [0:NumPipeRegs]               valid_q;
-  logic [0:NumPipeRegs]               ready_q;
+  // Input signals for the next stage (= output signals of the previous stage)
+  logic               [NumPipeRegs:0][Width-1:0] result_d;
+  fpnew_pkg::status_t [NumPipeRegs:0]            status_d;
+  logic               [NumPipeRegs:0]            extension_bit_d;
+  TagType             [NumPipeRegs:0]            tag_d;
+  AuxType             [NumPipeRegs:0]            aux_d;
+  logic               [NumPipeRegs:0]            valid_d;
+  // Ready signal is combinatorial for all stages
+  logic               [NumPipeRegs:0]            stage_ready;
 
   // Input stage: First element of pipeline is taken from inputs
-  assign result_q[0]        = result_i;
-  assign status_q[0]        = status_i;
-  assign extension_bit_q[0] = extension_bit_i;
-  assign tag_q[0]           = tag_i;
-  assign aux_q[0]           = aux_i;
-  assign valid_q[0]         = in_valid_i;
+  assign result_d[0]        = result_i;
+  assign status_d[0]        = status_i;
+  assign extension_bit_d[0] = extension_bit_i;
+  assign tag_d[0]           = tag_i;
+  assign aux_d[0]           = aux_i;
+  assign valid_d[0]         = in_valid_i;
 
-  // Input stage: Propagate pipeline ready signal
-  assign in_ready_o = ready_q[0];
+  // Input stage: Propagate ready signal from pipeline
+  assign in_ready_o = stage_ready[0];
 
-  // Generate pipeline stages as given by NumPipeRegs. When NumPipeRegs is 0, no registers are
-  // generated.
-  //            +---------|---------|---------|----------|-------------+
-  //            |         |         |         |          |             |
-  // inputs_i   >=========|=========|=========|====~~====|=============> inputs_o
-  // in_valid_i >---------|---------|---------|----~~----|-------------> out_valid_o
-  // in_ready_o <---------|---------|---------|----~~----|-------------< out_ready_i
-  //            |         |         |         |          |             |
-  // stage #    +----0----|----1----|----2----|----..----|-NumPipeRegs-+
-  //
-  // NOTE: These registers must be retimed in synthesis for sensible pipelining. Make sure to
-  // optimize registers through the instantiating hierarchy.
-  // The ready signal is not a direct feed-through from destination to source but takes into account
-  // intermediate 'bubbles' in the pipeline. As such, downstream stalls can be hidden when the
-  // pipeline is not full.
-  // Enable signals on the registers will lead to clock-gated pipeline stages when this optimization
-  // is enabled during synthesis. Make sure to optimize clock gates through hierarchies.
-  for (genvar i = 0; i < NumPipeRegs; i++) begin : pipeline_stages
-    // Internal register enable for this stage
-    logic reg_ena;
+  // Generate the pipeline stages in case they are needed
+  if (NumPipeRegs > 0) begin : gen_pipeline
+    // Pipelined versions of signals for later stages
+    logic               [NumPipeRegs-1:0][Width-1:0] result_q;
+    fpnew_pkg::status_t [NumPipeRegs-1:0]            status_q;
+    logic               [NumPipeRegs-1:0]            extension_bit_q;
+    TagType             [NumPipeRegs-1:0]            tag_q;
+    AuxType             [NumPipeRegs-1:0]            aux_q;
+    logic               [NumPipeRegs-1:0]            valid_q;
 
-    // Determine the ready signal of the current stage - advance the pipeline:
-    // 1. if the next stage is ready for our data
-    // 2. if the next stage only holds a bubble (not valid) -> we can pop it
-    assign ready_q[i] = ready_q[i+1] | ~valid_q[i+1];
+    for (genvar i = 0; i < NumPipeRegs; i++) begin : pipeline_stages
+      // Internal register enable for this stage -> creates gated registers if supported in synth
+      logic reg_ena;
 
-    // Valid: enabled by ready signal, synchronous clear with the flush signal
-    `FFLARNC(valid_q[i+1], valid_q[i], ready_q[i], flush_i, 1'b0, clk_i, rst_ni)
+      // Next state from previous register to form a shift register
+      assign result_d[i+1]        = result_q[i];
+      assign status_d[i+1]        = status_q[i];
+      assign extension_bit_d[i+1] = extension_bit_q[i];
+      assign tag_d[i+1]           = tag_q[i];
+      assign aux_d[i+1]           = aux_q[i];
+      assign valid_d[i+1]         = valid_q[i];
 
-    // Enable register if pipleine ready and a valid data item is present
-    assign reg_ena = ready_q[i] & valid_q[i];
+      // Determine the ready signal of the current stage - advance the pipeline:
+      // 1. if the next stage is ready for our data
+      // 2. if the next stage register only holds a bubble (not valid) -> we can pop it
+      assign stage_ready[i] = stage_ready[i+1] | ~valid_q[i];
 
-    // Generate the pipeline registers within the stages, use enable-registers
-    `FFL(result_q[i+1],        result_q[i],        reg_ena, '0)
-    `FFL(status_q[i+1],        status_q[i],        reg_ena, '0)
-    `FFL(extension_bit_q[i+1], extension_bit_q[i], reg_ena, '0)
-    `FFL(tag_q[i+1],           tag_q[i],           reg_ena, '0)
-    `FFL(aux_q[i+1],           aux_q[i],           reg_ena, '0)
+      // Valid registers: enabled by ready signal, synchronous clear with the flush signal
+      `FFLARNC(valid_q[i], valid_d[i], stage_ready[i], flush_i, 1'b0, clk_i, rst_ni)
+
+      // Enable the payload registers if pipleine ready and a valid data item is present (gating)
+      assign reg_ena = stage_ready[i] & valid_d[i];
+
+      // Generate the pipeline registers within the stages, use enable-registers
+      `FFL(result_q[i],        result_d[i],        reg_ena, '0)
+      `FFL(status_q[i],        status_d[i],        reg_ena, '0)
+      `FFL(extension_bit_q[i], extension_bit_d[i], reg_ena, '0)
+      `FFL(tag_q[i],           tag_d[i],           reg_ena, '0)
+      `FFL(aux_q[i],           aux_d[i],           reg_ena, '0)
+    end
   end
 
-  // Output stage: bind pipeline outputs to module output. Directly connects to input if no regs.
-  assign result_o        = result_q[NumPipeRegs];
-  assign status_o        = status_q[NumPipeRegs];
-  assign extension_bit_o = extension_bit_q[NumPipeRegs];
-  assign tag_o           = tag_q[NumPipeRegs];
-  assign aux_o           = aux_q[NumPipeRegs];
-  assign out_valid_o     = valid_q[NumPipeRegs];
+  // Output stage: bind last stage outputs to module output. Directly connects to input if no regs.
+  assign result_o        = result_d[NumPipeRegs];
+  assign status_o        = status_d[NumPipeRegs];
+  assign extension_bit_o = extension_bit_d[NumPipeRegs];
+  assign tag_o           = tag_d[NumPipeRegs];
+  assign aux_o           = aux_d[NumPipeRegs];
+  assign out_valid_o     = valid_d[NumPipeRegs];
 
   // Output stage: Ready travels backwards from output side
-  assign ready_q[NumPipeRegs] = out_ready_i;
+  assign stage_ready[NumPipeRegs] = out_ready_i;
 
   // The pipeline is considered busy if any valid data is in flight
-  assign busy_o = (| valid_q);
+  assign busy_o = (| valid_d);
 
 endmodule
