96d8b28a6f664db92c8a01778c9e1e21a417e4bb
Stefan Mach <smach@iis.ee.ethz.ch>
2019-01-22 21:39:55 +0100

    :bug: [round] Fix RUP/RDN rounding exact results

--- src/fpnew_rounding.sv
+++ src/fpnew_rounding.sv
@@ -52,9 +52,9 @@ module fpnew_rounding #(
           2'b11: round_up = 1'b1;           // > ulp/2 away, round up
           default: round_up = 1'bx; // propagate X
         endcase
-      fpnew_pkg::RTZ: round_up = 1'b0;                   // always round down
-      fpnew_pkg::RDN: round_up = sign_i;                 // round towards zero if +, away if -
-      fpnew_pkg::RUP: round_up = ~sign_i;                // round towards zero if -, away if +
+      fpnew_pkg::RTZ: round_up = 1'b0; // always round down
+      fpnew_pkg::RDN: round_up = (| round_sticky_bits_i) ? sign_i  : 1'b0; // to 0 if +, away if -
+      fpnew_pkg::RUP: round_up = (| round_sticky_bits_i) ? ~sign_i : 1'b0; // to 0 if -, away if +
       fpnew_pkg::RMM: round_up = round_sticky_bits_i[1]; // round down if < ulp/2 away, else up
       default: round_up = 1'bx; // propagate x
     endcase
