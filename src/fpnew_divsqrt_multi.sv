cb6a570abafc96e1b5a1237c9877bf2bc470edf6
Zeeshan Rafique <zeeshanrafique23@gmail.com>
2021-12-11 12:15:27 +0500

    [src/fpnew_divsqrt_multi] Resolve busy signal bug issue #53
    Signed-off-by: Zeeshan Rafique <zeeshanrafique23@gmail.com>

--- src/fpnew_divsqrt_multi.sv
+++ src/fpnew_divsqrt_multi.sv
@@ -203,7 +203,7 @@ module fpnew_divsqrt_multi #(
             state_d = IDLE; // we anticipate going back to idling..
             if (in_valid_q && unit_ready) begin // ..unless new work comes in
               in_ready = 1'b1; // we acknowledge the instruction
-              state_d  = BUSY; // and stay busy with it
+              state_d  = IDLE; // state change to idle
             end
           // Otherwise if downstream is not ready for the result
           end else begin
